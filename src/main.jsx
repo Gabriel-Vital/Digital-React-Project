4ec0f7ff3f2062fb7a4a5bc79374670b606d12c4
Gabriel <gabrielvital2710@gmail.com>
2025-06-14 10:08:12 -0300

    removed unused imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/Login.jsx";
 import TransactionsPage from "./pages/Transactions.jsx";
 import TransactionDetails from "./pages/TransactionDetails.jsx";
